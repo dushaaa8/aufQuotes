1882c5d8f804a13ed8eca3b7e70e9cfe84014795
dyshaaa <dyshakrut@gmail.com>
2025-06-17 16:53:01 +0300

    some fixes

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const startBtn = document.getElementById("start")
     let quoteContainer = document.getElementById("quote")
 
-    startBtn.addEventListener("click",async function () {
+    startBtn.addEventListener("click",async function () { // async is important for fatch!!!
         try {
-            const response = await fetch("https://api.adviceslip.com/advice?t="+ Date.now());
-            const data = await response.json();
-            quoteContainer.innerText = data.slip.advice;
+            const response = await fetch("https://api.adviceslip.com/advice?t="+ Date.now()); // getting object form API
+            const data = await response.json(); // server obj -> js obj
+            quoteContainer.innerText = data.slip.advice; // text from js obj
         } catch (error) {
             console.error("ERR", error);
-            quoteContainer.innerText = "oops...";
+            quoteContainer.innerText = "oops..."; // err
         }
 
     })
